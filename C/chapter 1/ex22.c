0661ccf0e1837ff1b75bb925f2b21ecbe53bfb7d
Guilherme Rosseti <66320361+DarkEyeBr@users.noreply.github.com>
2023-03-23 17:03:19 -0300

    second commit ahuahuah
    - aloisinho

---
+++ C/chapter 1/ex22.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    float valorInicial, valorFinal;
+    printf("Valor do produto: ");
+    scanf("%f", &valorInicial);
+    valorFinal = valorInicial*0.91;
+    printf("Preço de venda com 9%% de desconto: %.2f", valorFinal);
+    return 0;
+}
---
+++ C/chapter 1/ex23.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int 
+    anoNasc,
+    anoAtual;
+    printf("Ano de Nascimento: ");
+    scanf("%d", &anoNasc);
+    printf("Ano Atual: ");
+    scanf("%d", &anoAtual);
+
+    int idade = anoAtual-anoNasc;
+
+    printf("Idade aproximada: %d", idade);
+
+    return 0;
+}
