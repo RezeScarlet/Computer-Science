99f60bfc0f5c97dfbf966cd040de2934d29c21a5
Guilherme Rosseti <66320361+DarkEyeBr@users.noreply.github.com>
2023-03-28 14:53:12 -0300

    ex2.9 done

--- C/chapter 2/ex9.c
+++ C/chapter 2/ex9.c
@@ -5,10 +5,24 @@ int main() {
   int
   quantL;
   float
-  medidaL;
+  medidaL,
+  perimetro;
+
   printf("Entre com a quantidade de lados: ");
   scanf("%d", &quantL);
-
+  printf("Entre com a medida do lado: ");
+  scanf("%f", &medidaL);
   
+  if (quantL == 3){
+    printf("Triangulo de perimetro: %.2f", quantL*medidaL);
+  } else if (quantL == 4) {
+    printf("Quadrado de area: %.2f", medidaL*medidaL);
+  } else if (quantL == 5) {
+    printf("Pentagono");
+  } else {
+    printf(" Poligono nao identificado");
+
+  }
+
   return 0;
 }
