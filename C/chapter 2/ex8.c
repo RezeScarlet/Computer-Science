c543aef99f94f22b7fd56021075d6d5d66c476e2
Guilherme Rosseti <66320361+DarkEyeBr@users.noreply.github.com>
2023-03-28 14:32:21 -0300

    ex2.8 done

---
+++ C/chapter 2/ex8.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  float
+  n1,
+  n2,
+  n3,
+  t;
+
+  printf("N1: ");
+  scanf("%f", &n1);
+  printf("N2: ");
+  scanf("%f", &n2);
+  printf("N3: ");
+  scanf("%f", &n3);  
+
+  if (n1<n3) {
+    t = n1;
+    n1 = n3;
+    n3 = t;
+  }
+  if (n1<n2) {
+    t = n2;
+    n2 = n1;
+    n1 = t;
+  }
+  if (n2<n3) {
+    t = n2;
+    n2 = n3;
+    n3 = t;
+  }
+  
+
+  printf("A soma dos dois numeros maiores fornecidos e: %.2f", n1+n2);
+  scanf("%f", &);
+  
+  return 0;
+}
---
+++ C/chapter 2/ex9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int
+  quantL;
+  float
+  medidaL;
+  printf("Entre com a quantidade de lados: ");
+  scanf("%d", &quantL);
+
+  
+  return 0;
+}
