b040c39a5dbf85094a9293580b0b875bc37fea02
Guilherme Rosseti <66320361+DarkEyeBr@users.noreply.github.com>
2023-04-25 13:58:48 -0300

    Atualizar o desafio4.1.c

---
+++ C/chapter 4/desafio4.1.c
@@ -0,0 +1,74 @@
+// Feito por Guilherme Rosseti
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+ int troca;
+	int array[10];
+	int quantPar = 0;
+ int quantImp = 0;
+	
+	for (int i = 0; i < 10; i++) {
+		printf("array[%d]: ", i);
+		scanf("%d", &array[i]);
+	}
+
+for (int i = 0; i < 9; i++) {
+ for (int j = 0; j < i; j++){
+  if (array[j] > array[j+1]) {
+   troca = array[j];
+   array[j] = array[j+1];
+   array[j+1] = troca;
+  }
+ }
+}
+for (int i = 0; i < 10; i++) {
+			printf("\n %d", array[i]);
+		
+	}
+
+for (int i = 0; i < 20; i++) {
+if (i<10) {
+		if (array[i] % 2 == 0) {
+	 	quantPar++;
+						 
+		} else {
+    quantImp++;
+   }
+	}
+}
+  int arrayPar[quantPar];
+  int arrayImp[quantImp];
+
+
+ 
+
+	printf("Numeros pares:");
+ if (quantPar>0) {
+ for (int i = 0; i < quantPar; i++) {
+			printf(" %d", arrayPar[i]);
+		
+	}
+
+ } else {
+		printf(" nao ha");
+		
+	}
+	printf(".\n");
+	
+	printf("Numeros impares:");
+	if (quantImp>0) {
+ for (int i = 0; i < quantImp; i++) {
+			printf(" %d", arrayImp[i]);
+		
+	}
+
+ } else {
+		printf(" nao ha");
+		
+	}
+	printf(".");
+	
+	
+	return 0;
+}
