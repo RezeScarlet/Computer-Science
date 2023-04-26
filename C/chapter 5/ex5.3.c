a8696212cfb66d8dfef6f81913453f52488ce380
DarkEyeBr <gui171204@gmail.com>
2023-04-26 11:31:57 -0300

    5.4 done

--- C/chapter 5/ex5.3.c
+++ C/chapter 5/ex5.3.c
@@ -3,16 +3,36 @@
 #include <stdlib.h>
 
 int main() {
+	int mult;
 	int array[3][4];
 	int arrayMult[3][4];
+	
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 4; j++) {
+			printf("array[%d][%d]: ", i, j);
+			scanf("%d", &array[i][j]);
+
+		}
+	}
+	printf("Multiplicar por: ");
+	scanf("%d", &mult);
 
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 4; j++) {
-			printf("");
+			arrayMult[i][j] = array[i][j] * mult;
+
 			
 		}
-		
 	}
-	
+		printf("arrayMult:\n");
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 4; j++) {
+				printf("%03d ", arrayMult[i][j]);
+			
+			}
+			printf("\n");
+			
+		}
 	return 0;
 }
