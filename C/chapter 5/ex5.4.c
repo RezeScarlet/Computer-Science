c5cf3c3b88400c4be0661e2f803a60a62507dff3
DarkEyeBr <gui171204@gmail.com>
2023-04-26 12:18:09 -0300

    .

--- C/chapter 5/ex5.4.c
+++ C/chapter 5/ex5.4.c
@@ -3,6 +3,19 @@
 #include <stdlib.h>
 
 int main() {
+	int matriz[2][2];
+	int determinante;
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++) {
+			printf("array[%d][%d]: ", i, j);
+			scanf("%d", &matriz[i][j]);
+			
+			
+		}
+		
+	}
+	printf("");
 	
 	return 0;
 }
