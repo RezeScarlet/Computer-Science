433d6c2c1ebfd0b59244cb0a47096afddb615f7d
Guilherme Rosseti <66320361+DarkEyeBr@users.noreply.github.com>
2023-05-02 22:35:42 -0300

    .

--- C/cap5/ex5.8.c
+++ C/cap5/ex5.8.c
@@ -11,19 +11,38 @@ int main() {
 	if (valor <= 0 || valor > 100) {
 		printf("Numero incorreto!");
 		
-	} else {
-		printf("\ncomeço\n");
+	} else {	
+
+		int array[valor][valor];
+		int fim = valor;
+		int comeco = 0;
+
+		for (int i = 0; i < valor; i++) {
+			for (int j = 0; j < valor; j++) {
+				if (j >= comeco && j < fim) {
+					array[i][j] = i+1;
+
+				}
+			}
+		}
+		comeco++;
+		fim--;
+		
+		printf("\n");
 		
 		for (int i = 0; i < valor; i++) {
 			for (int j = 0; j < valor; j++) {
-				printf("  1 ");
+				printf("  %d", array[i][j]);
+				if (j != valor-1) {
+					printf(" ");
+					
+				}
+				
 				
 			}
 			printf("\n");
 			
-		}
-		printf("\nfim\n");
-		
+		}		
 	}
 	
 	
