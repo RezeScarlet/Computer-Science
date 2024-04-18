147ac2ae05881bcc7fddec9770616e7d503760c2
RezeScarlet <gui171204@gmail.com>
2024-04-17 22:44:23 -0300

    third ex complete

--- Programming Marathon/AbovetheMainDiagonal.cpp
+++ Programming Marathon/AbovetheMainDiagonal.cpp
@@ -2,46 +2,31 @@
 using namespace std;
 int main() {
   char operation;
-  double M[12][12];
+  double M[12][12], sum = 0;
   cin >> operation;
-  for (int i = 0; i < 11; i++) {
-    for (int j = 0; j < 11; j++) {
+  for (int i = 0; i <= 11; i++) {
+    for (int j = 0; j <= 11; j++) {
       cin >> M[i][j];
+      // M[i][j] = 2;
     }
   }
 
+  for (int i = 1; i <= 11; i++) {
+    for (int j = 0; j < i; j++) {
+      sum += M[j][i];
+      // cout << sum << endl;
+      // cout << "[" << j << "][" << i << "]" << "  ";
+    }
+    // cout << endl;
+  }
   switch (operation) {
   case 'S':
-
-    for (int i = 0; i < 11; i++) {
-      for (int j = 0; j < 11; j++) {
-        cin >> M[i][j];
-      }
-    }
+    cout << sum << endl;
     break;
   case 'M':
+    double ave = sum / 66;
+    cout << ave << endl;
     break;
   }
-  int A, B;
-  double G1, G2, time;
-
-  cin >> A >> B >> G1 >> G2;
-
-  G1 = 1 + G1 / 100;
-
-  G2 = 1 + G2 / 100;
-
-  for (time = 0; A <= B; time++) {
-    A = A * G1;
-    B = B * G2;
-    if (time > 100) {
-      break;
-    }
-  }
-
-  if (time > 100) {
-    cout << "Mais de 1 seculo.\n";
-  } else
-    cout << time << " anos.\n";
   return 0;
 }
